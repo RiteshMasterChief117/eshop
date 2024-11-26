34d5790b063c052809dd81613e1d118670930bb1
RiteshMasterChief117 <59278642+RiteshMasterChief117@users.noreply.github.com>
2024-11-27 02:39:14 +0530

    product api view

--- eshop/settings.py
+++ eshop/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'product',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'product.apps.ProductConfig',
 ]
 
 MIDDLEWARE = [
